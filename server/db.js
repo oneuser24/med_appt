cacfabba1a521f2bfe58e0fe68317ad2cf44de14
oneuser24 <bohdan.senyuk@colorado.edu>
2026-09-16 23:10:46 -0600

    set up database

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:q2rjTbon0uQzxGGLyAmXH25L@172.21.208.164:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
