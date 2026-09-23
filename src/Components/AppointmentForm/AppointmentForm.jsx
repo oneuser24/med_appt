159b30eb533b852f96c44b2039294d1d93c83fff
oneuser24 <bohdan.senyuk@colorado.edu>
2026-09-22 22:02:47 -0600

    Create AppointmentForm component

--- src/Components/AppointmentForm/AppointmentForm.jsx
+++ src/Components/AppointmentForm/AppointmentForm.jsx
@@ -1,8 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+
+const timeSlots = ['08:00 AM',
+                  '08:30 AM',
+                  '09:00 AM',
+                  '09:30 AM',
+                  '10:00 AM',
+                  '10:30 AM',
+                  '11:00 AM',
+                  '11:30 AM',
+                  '01:00 PM',
+                  '01:30 PM',
+                  '02:00 PM',
+                  '02:30 PM',
+                  '03:00 PM',
+                  '03:30 PM',
+                  '04:00 PM',
+                  '04:30 PM'];
 
 const AppointmentForm = ({ doctorName, doctorSpeciality, onSubmit }) => {
     const [name, setName] = useState('');
     const [phoneNumber, setPhoneNumber] = useState('');
+    const [apptDate, setApptDate] = useState('');
     const [selectedSlot, setSelectedSlot] = useState(null);
 
     const handleSlotSelection = (slot) => {
@@ -11,10 +29,22 @@ const AppointmentForm = ({ doctorName, doctorSpeciality, onSubmit }) => {
 
     const handleFormSubmit = (e) => {
       e.preventDefault();
-      onSubmit({ name, phoneNumber });
+      onSubmit({ name, phoneNumber,  apptDate, selectedSlot});
       setName('');
       setPhoneNumber('');
+      setApptDate('');
+      setSelectedSlot('');
     };
+
+    const [currentDate, setCurrentDate] = useState('');
+
+    useEffect(() => {
+      const today = new Date();
+      const year = today.getFullYear();
+      const month = String(today.getMonth() + 1).padStart(2, '0');
+      const day = String(today.getDate()).padStart(2, '0');
+      setCurrentDate(`${year}-${month}-${day}`);
+    }, []);
 
     return (
       <form onSubmit={handleFormSubmit} className="appointment-form">
@@ -36,7 +66,32 @@ const AppointmentForm = ({ doctorName, doctorSpeciality, onSubmit }) => {
             value={phoneNumber}
             onChange={(e) => setPhoneNumber(e.target.value)}
             required
+            pattern='[0-9]{10}'
           />
+        </div>
+        <div className="form-group">
+          <label htmlFor="apptDate">Date of Appointment:</label>
+          <input
+            type="date"
+            id="apptDate"
+            value={apptDate}
+            onChange={(e) => setApptDate(e.target.value)}
+            required
+            min={currentDate}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="apptTime">Time:</label>
+          <select
+            type="text"
+            id="selectedSlot"
+            value={selectedSlot}
+            onChange={(e) => handleSlotSelection(e.target.value)}
+            required
+          >
+            <option value=''>Select a time slot</option>
+            {timeSlots.map(timeSlot => <option value={timeSlot}>{timeSlot}</option>)}
+          </select>
         </div>
         <button type="submit">Book Now</button>
       </form>
